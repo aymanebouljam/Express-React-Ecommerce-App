c7b832a6d2b6895e9b52184e24e6c1813b363e15
Aymane <its.aymane.bouljam@gmail.com>
2025-07-02 23:05:08 +0100

    Add matchPassword method to users Schema methods

--- api/models/User.js
+++ api/models/User.js
@@ -1,14 +1,18 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-module.exports = model(
-  'User',
-  new Schema(
-    {
-      name: { type: String, required: true },
-      email: { type: String, required: true, unique: true },
-      password: { type: String, required: true, selected: false },
-      isAdmin: { type: Boolean, default: false },
-    },
-    { timestamps: true },
-  ),
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true },
 );
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = model('User', userSchema);
