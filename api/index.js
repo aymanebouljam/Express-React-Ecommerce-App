b38b880d9623e32359823f71282c1729493ac22a
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-02 23:06:21 +0100

    Register error Handler in index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const errorHandler = require('./middlewares/errorHandler');
+const usersRouter = require('./routes/usersRoutes');
 
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 9000;
 const MONGO_URL = process.env.MONGO_URL;
+
+// Connection to Database
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log('Connected to MongoDB Database'))
+  .catch((error) => console.error('Failed to connect to Database: ', error));
 
 // Test route
 app.get('/', (req, res) => {
   res.send('app is running');
 });
 
-mongoose
-  .connect(MONGO_URL)
-  .then(() => console.log('Connected to MongoDB Database'))
-  .catch((error) => console.error(error));
+// Users Routes
+app.use('/api/users', usersRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
