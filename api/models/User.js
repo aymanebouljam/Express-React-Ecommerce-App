dd06a6a4da247229c77c1a6df39fb31607db579a
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-03 00:06:31 +0100

    Create pre save middlware to hash the password if changed or newly added

--- api/models/User.js
+++ api/models/User.js
@@ -15,4 +15,12 @@ userSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 module.exports = model('User', userSchema);
