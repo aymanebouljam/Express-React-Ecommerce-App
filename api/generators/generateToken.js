f20a927693d06f2e0a38ac4f8b15026dbc27df2d
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-03 02:12:04 +0100

    generate jwt token in Login Route

--- api/generators/generateToken.js
+++ api/generators/generateToken.js
@@ -1,4 +1,4 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-module.exports = (id, name) => jwt.sign({ id, name }, process.env.privateKey, { expiresIn: '15d' });
+module.exports = (id, name) => jwt.sign({ id, name }, process.env.JWT_SECRET, { expiresIn: '15d' });
