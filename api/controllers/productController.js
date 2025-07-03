66996e7f41a49bedc251365e98ede47b2007c318
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-03 20:32:27 +0100

    Create Single product controller & GET router

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,7 +1,33 @@
 const Product = require('../models/Product');
+const { ObjectId } = require('mongoose').Types;
 
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
 
   return res.status(200).json(products);
+};
+
+exports.getSingleProduct = async (req, res) => {
+  const { id } = req.params;
+
+  console.log(req.params);
+
+  if (!id) {
+    res.status(400);
+    throw new Error('No Product ID was provided');
+  }
+
+  if (!ObjectId.isValid(id)) {
+    res.status(400);
+    throw new Error('Invalid Product ID');
+  }
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error('Product Not Found');
+  }
+
+  return res.status(200).json(product);
 };
