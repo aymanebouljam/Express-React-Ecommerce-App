4305cfdaa5763203c936a87daa2fc10ef8d98d43
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-03 19:32:06 +0100

    Create Profile Update controller and route

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const handler = require('express-async-handler');
-const { login, register, getProfile } = require('../controllers/userController');
+const { login, register, getProfile, updateProfile } = require('../controllers/userController');
 const auth = require('../middlewares/auth');
 
 router.post('/login', handler(login));
@@ -8,5 +8,7 @@ router.post('/login', handler(login));
 router.post('/', handler(register));
 
 router.get('/profile', auth, handler(getProfile));
+
+router.patch('/profile', auth, handler(updateProfile));
 
 module.exports = router;
