019f0155274137d51bc577f05dcfc37cab1ade73
Aymane <its.aymane.bouljam@gmail.com>
2025-07-04 17:22:03 +0100

    Added Routes to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import Products from "./components/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/details" element={<ProductDetails />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
