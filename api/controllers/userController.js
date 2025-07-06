d02b6f649eb11ffa51b15f8da4c3d253b2bf9419
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-06 14:20:37 +0100

    Update json response from login & register controller to structure it into user & token only

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -12,17 +12,12 @@ exports.login = async (req, res) => {
   }
 
   return res.status(200).json({
-    id: user._id,
-    name: user.name,
-    email: user.email,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
     token: generateToken(user._id, user.name),
-    createdAt: new Date(user.createdAt).toLocaleString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-    }),
   });
 };
 
@@ -39,13 +34,20 @@ exports.register = async (req, res) => {
     throw new Error('A user was already registered with this email');
   }
 
-  await new User({
+  const user = await new User({
     name,
     email,
     password,
   }).save();
 
-  return res.status(201).json({ message: 'User registered successfully' });
+  return res.status(201).json({
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    token: generateToken(user._id, user.name),
+  });
 };
 
 exports.getProfile = async (req, res) => {
