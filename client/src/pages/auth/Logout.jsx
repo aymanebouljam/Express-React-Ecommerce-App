18c1c77154c0f2b6aa1e3816ee218a1463afc522
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-06 17:22:22 +0100

    Add Logout page

--- client/src/pages/auth/Logout.jsx
+++ client/src/pages/auth/Logout.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-  return <div>Logout</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(logout());
+    navigate("/login");
+  }, [dispatch, navigate]);
+
+  return null;
 };
 
 export default Logout;
