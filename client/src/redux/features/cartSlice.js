b49a9a978d195b8bcf4943277cad440e0577b4e2
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-06 23:09:13 +0100

    Create Cart slice & reducers

---
+++ client/src/redux/features/cartSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: JSON.parse(localStorage.getItem("cartItems")) || [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find((i) => i.id === item.id);
+
+      if (existingItem) {
+        existingItem.quantity += item.quantity || 1;
+      } else {
+        state.items.push({ ...item, quantity: item.quantity || 1 });
+      }
+
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
+    },
+
+    clearCart: (state) => {
+      state.items = [];
+      localStorage.removeItem("cartItems");
+    },
+
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+
+      const item = state.items.find((item) => item.id === id);
+
+      if (item && quantity > 0) {
+        item.quantity = quantity;
+      } else if (item && quantity === 0) {
+        state.items = state.items.filter((item) => item.id !== id);
+      }
+
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart, updateQuantity } =
+  cartSlice.actions;
+export default cartSlice.reducer;
