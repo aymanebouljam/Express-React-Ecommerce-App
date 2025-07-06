edf586c44d1b5a3c07658d01a78883e09798d47a
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-06 22:25:31 +0100

    Hide login & register buttons for authenticated users & show logout only

--- client/src/layouts/Navbar.jsx
+++ client/src/layouts/Navbar.jsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
+
   return (
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a className="flex items-center space-x-3 rtl:space-x-reverse">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
+          <Link
+            to="/"
+            className="flex items-center space-x-1 rtl:space-x-reverse"
+          >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="https://www.svgrepo.com/show/415710/ecommerce-price-price-tag.svg"
               className="h-8"
-              alt="Flowbite Logo"
+              alt="Shopy Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Shopy
             </span>
-          </a>
+          </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Get started
-            </button>
+            {token ? (
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={() => navigate("/logout")}
+              >
+                Logout
+              </button>
+            ) : (
+              <>
+                <button
+                  type="button"
+                  className="text-blue-700 bg-none hover:text-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-3"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </button>
+              </>
+            )}
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
