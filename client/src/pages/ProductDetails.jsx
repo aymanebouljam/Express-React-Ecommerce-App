a638792050fc123360327e5e295c663db521b752
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-09 19:33:51 +0100

    Added add to Cart to product Detail page

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -17,8 +17,7 @@ const ProductDetails = () => {
 
   const [localProduct, setLocalProduct] = useState(null);
   const [quantity, setQuantity] = useState(0);
-  const [disabled, setDisabled] = useState(false);
-  
+  const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
     if (!id) return;
@@ -64,9 +63,12 @@ const ProductDetails = () => {
   const handleQuantity = (e) => {
     const qte = e.target.value;
 
-    if (qte <= product.countInStock) {
-      setDisabled(false);
+    if (qte < 1) {
+      setQuantity(0);
+      setDisabled(true);
+    } else if (qte <= product.countInStock) {
       setQuantity(qte);
+      setDisabled(false);
     } else {
       setDisabled(true);
     }
@@ -76,15 +78,15 @@ const ProductDetails = () => {
       dispatch(
         addToCart({
           name: product.name,
-          quantity: quantity,
+          quantity: Number(quantity),
           image: product.image,
-          price: product.price,
+          price: Number(product.price),
           product: product._id,
         })
       );
-    }else{
-       alert('You need to choose a quantity !')
-    }   
+    } else {
+      alert("You need to choose a quantity !");
+    }
   };
 
   return (
@@ -222,7 +224,6 @@ const ProductDetails = () => {
                     className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base pl-3 w-1/4"
                     value={quantity}
                     onChange={handleQuantity}
-                    disabled={product.countInstock === 0 ? true : disabled}
                   />
                 </div>
               </div>
@@ -231,14 +232,19 @@ const ProductDetails = () => {
                   ${product.price}
                 </span>
                 <button
-                  className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded"
+                  className={`flex ml-auto ${
+                    disabled
+                      ? "text-dark bg-gray-100 hover:bg-gray-200"
+                      : "text-white bg-blue-500 hover:bg-blue-600"
+                  } border-0 py-2 px-6 focus:outline-none  rounded`}
                   onClick={() => {
-                    handleAddToCart(product._id);
+                    handleAddToCart();
                   }}
+                  disabled={disabled}
                 >
                   Add to cart
                 </button>
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4" disabled = {disabled}>
+                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
