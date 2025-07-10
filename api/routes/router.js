a7470cb93fd89a9c9322bc7fd91d4349e9d8eaec
Aymane Bouljam <its.aymane.bouljam@gmail.com>
2025-07-10 15:54:26 +0100

    Add routes & controller to retrieve taxes & shipping details

--- api/routes/router.js
+++ api/routes/router.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const productsRoutes = require('./productsRoutes');
 const ordersRoutes = require('./ordersRoutes');
+const billingRoutes = require('./billingRoutes');
 
 usersRoutes(router);
 
 productsRoutes(router);
 
 ordersRoutes(router);
+
+billingRoutes(router);
 
 module.exports = router;
